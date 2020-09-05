50accc5239210295479217864b02901ea1a74f0c
dawa1004 <wr1004wr1004wr@gmail.com>
2020-09-05 19:04:11 +0900

    fin commit

--- makepdf.php
+++ makepdf.php
@@ -6,7 +6,10 @@ $pdf = new tFPDF; // tFPDFのインスタンス作成
 $pdf->AddFont('ShipporiMincho','','ShipporiMincho-TTF-Regular.ttf',true);
 
 
-$names = array('相田 孝雄','池浦 隆','石田 房江')
+// GETパラメータからカンマ区切りで配列として取得
+// $names = explode(",", $_GET['names']);
+$names = htmlentities($_GET['names'],ENT_QUOTES,"utf-8");
+$names = explode("\r\n",$names);
 
 foreach ($names as $name) {
   /* ループする処理
@@ -27,125 +30,25 @@ $pdf->Output(); // pdfを出力する
 
 function make_contents() {
   global $pdf; // $pdfは関数の外で宣言されているため、global $pdf;と宣言する
-
+  // $contents = explode(",", $_GET['contents']); GETパラメータからカンマ区切りで配列として取得し、変数$contentsに代入
+  $contents = htmlentities($_GET['contents'],ENT_QUOTES,"utf-8"); // GETはそのまま扱うとセキュリティ上のリスクあるので、htmlentitiesを用いてエスケープする
+  $contents = explode("\r\n",$contents); // 改行ごとに分解して配列として取得
+  $count = 0; // ループ回数を数える
   $Y = $pdf->getY();
 
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(1)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(80,10,"1 + 1 =");
-  $pdf->Ln(25);
-
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(2)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(80,10,"1 + 2 =");
-  $pdf->Ln(25);
-
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(3)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(80,10,"1 + 3 =");
-  $pdf->Ln(25);
-
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(4)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(80,10,"1 + 4 =");
-  $pdf->Ln(25);
-
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(5)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(80,10,"1 + 5 =");
-  $pdf->Ln(25);
-
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(6)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(80,10,"1 + 6 =");
-  $pdf->Ln(25);
-
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(7)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(80,10,"1 + 7 =");
-  $pdf->Ln(25);
-
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(8)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(80,10,"1 + 8 =");
-  $pdf->Ln(25);
-
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(9)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(80,10,"1 + 9 =");
-  $pdf->Ln(25);
-
-  // X座標には先ほどと同じく110を、Y座標にはあらかじめ取得しておいた１行目のY座標をセット
-  $pdf->setXY(110,$Y); // setXY([X座標],[Y座標])
-
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(10)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(50,10,"2 + 1 =");
-  $pdf->Ln(25);
-
-  $pdf->setX(110);
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(11)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(50,10,"2 + 2 =");
-  $pdf->Ln(25);
-
-  $pdf->setX(110);
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(12)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(50,10,"2 + 3 =");
-  $pdf->Ln(25);
-
-  $pdf->setX(110);
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(13)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(50,10,"2 + 4 =");
-  $pdf->Ln(25);
-
-  $pdf->setX(110);
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(14)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(50,10,"2 + 5 =");
-  $pdf->Ln(25);
-
-  $pdf->setX(110);
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(15)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(50,10,"2 + 6 =");
-  $pdf->Ln(25);
-
-  $pdf->setX(110);
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(16)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(50,10,"2 + 7 =");
-  $pdf->Ln(25);
-
-  $pdf->setX(110);
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(17)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(50,10,"2 + 8 =");
-  $pdf->Ln(25);
-
-  $pdf->setX(110);
-  $pdf->SetFont('ShipporiMincho','',25);
-  $pdf->Cell(20,10,"(18)");
-  $pdf->SetFont('ShipporiMincho','',30);
-  $pdf->Cell(50,10,"2 + 9 =");
-  $pdf->Ln(25);
+  foreach($contents as $content){
+    $count++;
+    if($count == 10){
+      // $countが10になると2段目になる
+      $pdf->setY($Y);
+    }
+    if($count >= 10){
+      $pdf->setX(110);
+    }
+    $pdf->SetFont('ShipporiMincho','',25);
+		$pdf->Cell(20,10,"({$count})");
+		$pdf->SetFont('ShipporiMincho','',30);
+		$pdf->Cell(80,10,$content);
+		$pdf->Ln(25);
+  }
 }
